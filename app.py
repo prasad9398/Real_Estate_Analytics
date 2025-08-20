2ef140f8a97ac5562649938745c43f9d45c62031
Kolusu Veera Vara Prasad <prasadkolusu09@gmail.com>
2025-08-20 12:06:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 db_config = {
     'host': 'localhost',
     'user': 'root',
-    'password': 'Prasad@123', 
+    'password': 'Your Password', 
     'database': 'real_estate'
 }
 
